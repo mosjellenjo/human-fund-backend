d952d7e04e666eb87948f53266fe1965960ac8c3
Jo Bjordal <mosjellenjo@gmail.com>
2025-06-12 23:46:55 +0200

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,15 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet" />
+        {/* Favicon */}
+        <link rel="icon" href="/images/Original_logo_bw_humans3.png" type="image/png" />
+        {/* Structured data for Search logo */}
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: `{
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          "url": "https://humanfund.no",
+          "logo": "https://humanfund.no/images/Original_logo_bw_humans3.png"
+        }` }} />
       </head>
       <body suppressHydrationWarning>
         {children}
